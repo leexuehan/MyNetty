fe3de8336a0b7b188fad6cc1ac5dd75647bddbeb
leexuehan <xuehanlee@163.com>
2019-06-03 18:02:49 +0800

    add ignore

--- mynetty/src/Main.java
+++ mynetty/src/Main.java
@@ -1,10 +1,11 @@
 import common.NioServerSocketChannel;
 import threadmodel.NioEventLoop;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         NioServerSocketChannel serverSocketChannel = new NioServerSocketChannel();
         serverSocketChannel.setEventLoop(new NioEventLoop(10));
         serverSocketChannel.bind(new InetSocketAddress(9999));
