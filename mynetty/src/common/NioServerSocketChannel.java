1247bd4420338d85a3ad798f93b75049f4c52376
leexuehan <xuehanlee@163.com>
2019-06-03 17:31:54 +0800

    add serversocketchannel to nio channel

--- mynetty/src/common/NioServerSocketChannel.java
+++ mynetty/src/common/NioServerSocketChannel.java
@@ -2,10 +2,40 @@ package common;
 
 import threadmodel.NioEventLoop;
 
+import java.io.IOException;
 import java.net.SocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.spi.SelectorProvider;
 
 public class NioServerSocketChannel {
     private NioEventLoop eventLoop;
+    private static final SelectorProvider DEFAULT_SELECTOR_PROVIDER = SelectorProvider.provider();
+
+    //java nio channel
+    private ServerSocketChannel ch;
+
+    private final int readInterestOps;
+
+    public NioServerSocketChannel() {
+        SelectorProvider provider = DEFAULT_SELECTOR_PROVIDER;
+        ch = newChannel(provider);
+        try {
+            ch.configureBlocking(false);
+        } catch (IOException e) {
+            throw new ChannelException("failed to enter non-blocking mode", e);
+        }
+        this.readInterestOps = SelectionKey.OP_ACCEPT;
+
+    }
+
+    private ServerSocketChannel newChannel(SelectorProvider provider) {
+        try {
+            return provider.openServerSocketChannel();
+        } catch (IOException e) {
+            throw new ChannelException("open server socket channel exception");
+        }
+    }
 
     public void bind(final SocketAddress socketAddress) {
 
@@ -13,5 +43,9 @@ public class NioServerSocketChannel {
 
     public void setEventLoop(NioEventLoop eventLoop) {
         this.eventLoop = eventLoop;
+    }
+
+    public NioEventLoop getEventLoop() {
+        return eventLoop;
     }
 }
